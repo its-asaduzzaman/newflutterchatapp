42d34ba2b34e6168802117063cccaa87573678f4
its-asaduzzaman <samrat.asad@gmail.com>
2024-02-04 22:01:02 +0600

    TextField.dart and loginpage v0.1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:newflutterchatappwithfirebase/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            //welcome back message
+            Text(
+              "Welcome back. you've benn missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+            //email textField
+            const MyTextField(hintText: "Email"),
+
+            //pw TextField
+
+            //LoginButton
+
+            //Register Now
+          ],
+        ),
+      ),
     );
   }
 }
