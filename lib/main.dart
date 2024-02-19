afd7a02df04069b2405c58c702e64318e514caee
its-asaduzzaman <samrat.asad@gmail.com>
2024-02-19 12:38:43 +0600

    register page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newflutterchatappwithfirebase/pages/login_page.dart';
+import 'package:newflutterchatappwithfirebase/pages/register_page.dart';
 import 'package:newflutterchatappwithfirebase/themes/light_mode.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: lightMode,
-      home:  LoginPage(),
+      home:  RegisterPage(),
     );
   }
 }
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import '../components/my_button.dart';
+import '../components/my_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+  //emila pw controller
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  RegisterPage({Key? key}) : super(key: key);
+  //register method
+  void register() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            //welcome back message
+            Text(
+              "Let's create an account for you",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+            //email textField
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            //pw TextField
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _passwordController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            //pw TextField
+            MyTextField(
+              hintText: "Confirm Password",
+              obscureText: true,
+              controller: _confirmPasswordController,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //LoginButton
+
+            //Register Now
+            MyButton(
+              text: "Register",
+              onTap: register,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already have an account? ",
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                Text(
+                  "Login now,",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
