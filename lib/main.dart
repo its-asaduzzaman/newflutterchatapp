693b2e90bf3db72d028017ebad1584d781332a5d
Asaduzzaman_mac <samrat.asad@gmail.com>
2024-06-10 15:00:09 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:newflutterchatappwithfirebase/firebase_options.dart';
 import 'package:newflutterchatappwithfirebase/themes/light_mode.dart';
 import 'package:newflutterchatappwithfirebase/auth/login_or_register.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
